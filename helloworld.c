323900c091bd6ed6bd73dd533f26014147080972
haleighdefoor <77861608+haleighdefoor@users.noreply.github.com>
2021-03-31 13:38:17 -0400

    Create helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const int button =8;
+const int led =7;
+
+void setup() {
+  pinMode(button,INPUT);
+  pinMode(led,OUTPUT);
+  digitalWrite(led,LOW);
+}
+
+void loop() {
+  if (digitalRead(button)==HIGH){
+      digitalWrite(led,HIGH); 
+      delay(1000);    
+      digitalWrite(led, LOW); 
+      delay(1000);
+  }
+    else{
+      digitalWrite(led,LOW);
+    }
+}  
